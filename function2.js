a51b049f1a60e9318bd1cffa5bfef27c1ece3bdf
Eric Kuha <erickuha@gmail.com>
2024-04-11 13:03:50 -0500

    Updated

--- function2.js
+++ function2.js
@@ -2,8 +2,8 @@ function addTwo() {
     event.preventDefault();
     var a, b;
 
-    a = document.getElementById("num1").value;
-    b = document.getElementById("num2").value;
+    a = Number(document.getElementById("num1").value);
+    b = Number(document.getElementById("num2").value);
 
     var sum = a + b;
     document.getElementById("output").innerHTML = "<p>The Sum is: " + sum + "</p>";
